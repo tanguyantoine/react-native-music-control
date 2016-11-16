7815e2614f2c9ad64fccbac3d76a78bc8c03d6eb
Dante Cervantes <cegodai@gmail.com>
2016-11-16 15:06:15 -0600

    Can set artwork from URL or local file (absolute path )

--- android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
+++ android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
@@ -1,5 +1,6 @@
 package com.tanguyantoine.react;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Notification;
@@ -10,8 +11,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
 import android.media.AudioManager;
 import android.media.session.MediaSession;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -28,6 +34,12 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.ReadableMap;
 
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 
@@ -37,6 +49,7 @@ public class MusicControlModule extends ReactContextBaseJavaModule {
     private Notification.Builder notificationBuilder;
     private ReadableMap infos;
     private WritableNativeMap enabledControls;
+    public NotificationManager notificationManager;
 
     public static final String MUSIC_CONTROL_EVENT_NAME = "RNMusicControlEvent";
     public static final String ACTION_PLAY = "play";
@@ -136,16 +149,85 @@ public class MusicControlModule extends ReactContextBaseJavaModule {
         return new Notification.Action.Builder(icon, title, pendingIntent).build();
     }
 
+    // Get image from url
+    private Bitmap getBitmapCover(String coverURL){
+        Bitmap mybitmap;
+        try{
+            if(coverURL.matches("^(https?|ftp)://.*$"))
+                // Remote image
+                mybitmap = getBitmapFromURL(coverURL);
+            else{
+                // Local image
+                mybitmap = getBitmapFromLocal(coverURL);
+            }
+            return mybitmap;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    // get Local image
+    private Bitmap getBitmapFromLocal(String localURL){
+        try {
+            Uri uri = Uri.parse(localURL);
+            File file = new File(uri.getPath());
+            FileInputStream fileStream = new FileInputStream(file);
+            BufferedInputStream buf = new BufferedInputStream(fileStream);
+            Bitmap myBitmap = BitmapFactory.decodeStream(buf);
+            buf.close();
+            return myBitmap;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    // get Remote image
+    private Bitmap getBitmapFromURL(String strURL) {
+        try {
+            URL url = new URL(strURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
     private void updateNotification(){
         initNotificationBuilder();
+        //artwork uri : path absolute
+        String filePath = infos.getString("artwork");
+        Bitmap mybitmap = getBitmapCover(filePath);
 
-        this.notificationBuilder
-                .setSmallIcon(android.R.drawable.arrow_up_float);
+        this.notificationBuilder.setSmallIcon(android.R.drawable.ic_media_play);
+        //icono largo es bitmap
+        this.notificationBuilder.setLargeIcon(mybitmap);
         String title = infos.hasKey("title") ? infos.getString("title") : "";
         String content = infos.hasKey("artist") ? infos.getString("artist") : "";
         this.notificationBuilder
                 .setContentTitle(title)
                 .setContentText(content);
+
+        //open app if tapped
+        Intent resultIntent = new Intent(reactContext, this.getCurrentActivity().getClass());
+        resultIntent.setAction(Intent.ACTION_MAIN);
+        resultIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(reactContext, 0, resultIntent, 0);
+        this.notificationBuilder.setContentIntent(resultPendingIntent);
+
+        //notification can't be destroyed by swiping
+        //this.notificationBuilder.setOngoing(true);
+
+        //If 5.0 >= set the controls to be visible on lockscreen
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP){
+            this.notificationBuilder.setVisibility(Notification.VISIBILITY_PUBLIC);
+        }
 
         if(shouldActivateControl(CONTROL_previousTrack)){
             this.notificationBuilder
@@ -166,12 +248,16 @@ public class MusicControlModule extends ReactContextBaseJavaModule {
                     .addAction(generateAction(android.R.drawable.ic_media_next, "Next", ACTION_NEXT));
         }
 
+        this.notificationBuilder.setPriority(Notification.PRIORITY_MAX);
+
         notify(this.notificationBuilder);
-    }
-    private boolean shouldActivateControl(String controlName){
-        return enabledControls.hasKey(controlName) && enabledControls.getBoolean(controlName);
 
     }
+
+    private boolean shouldActivateControl(String controlName){
+        return enabledControls.hasKey(controlName) && enabledControls.getBoolean(controlName);
+    }
+
     private void initNotificationBuilder(){
         if(notificationBuilder == null) {
             Notification.MediaStyle style = new Notification.MediaStyle();
@@ -187,8 +273,12 @@ public class MusicControlModule extends ReactContextBaseJavaModule {
     }
 
     private void notify(Notification.Builder builder){
-        NotificationManager notificationManager = (NotificationManager) reactContext.getSystemService(Context.NOTIFICATION_SERVICE );
-        notificationManager.notify(NOTIFICATION_ID, builder.build());
+        this.notificationManager = (NotificationManager) reactContext.getSystemService(Context.NOTIFICATION_SERVICE );
+        this.notificationManager.notify(NOTIFICATION_ID, builder.build());
         this.notificationBuilder = null;
+    }
+
+    public void destroy(){
+        this.notificationManager.cancel(NOTIFICATION_ID);
     }
 }
