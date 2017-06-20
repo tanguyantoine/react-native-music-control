6513ef0f679e0fc73662962563d51ae6c1d7be49
Anton Filatov <anton.s.filatov@gmail.com>
2017-06-20 20:19:49 +0300

    Fixes #68 (#72)
    * check for notification is null on destroy()
    
    hard crash fix
    
    * check for notification is null on resetNowPlaying()

--- android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
+++ android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
@@ -127,8 +127,8 @@ public class MusicControlModule extends ReactContextBaseJavaModule implements Co
 
     public void destroy() {
         if(!init) return;
-        
-        notification.hide();
+
+        if (notification != null) notification.hide();
         session.release();
 
         ReactApplicationContext context = getReactApplicationContext();
@@ -285,7 +285,7 @@ public class MusicControlModule extends ReactContextBaseJavaModule implements Co
 
         md = new MediaMetadataCompat.Builder();
 
-        notification.hide();
+        if (notification != null) notification.hide();
         session.setActive(false);
     }
 
