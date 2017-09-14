tanguyantoine/react-native-music-control

commit d5a269e7a6016962203ba40446191b4560cef315

Add custom notification icon options for Android (#88)
* adds setCustomNotificationIcon method to MusicControlNotication and exposes it to React

* adds setCustomNotificationIcon method as a no-op on iOS

* adds JS method deffitinitions for setCustomNotificationIcon

* fixes the resource identfier for custom icon on Android to follow Android resource name requirement

* updates docs to incldue the new customizable icon info and example

* fixes the iOS dummy method's varibale type deffinition

* removes the setCustomNotificationIcon function in favor of using a notificationIcon prop in setNowPlaying

* Updates documentation to reflect the change from function to a prop in setNowPlaying

* simplifies the custom notification icon implementation

* matches code styling

android/src/main/java/com/tanguyantoine/react/MusicControlNotification.java
@@ -24,6 +24,7 @@ public class MusicControlNotification {
     private final MusicControlModule module;
 
     private int smallIcon;
+    private int customIcon;
     private NotificationCompat.Action play, pause, stop, next, previous, skipForward, skipBackward;
 
     public MusicControlNotification(MusicControlModule module, ReactApplicationContext context) {
@@ -34,8 +35,15 @@ public class MusicControlNotification {
         String packageName = context.getPackageName();
 
         // Optional custom icon with fallback to the play icon
-        smallIcon = r.getIdentifier("music-control-icon", "drawable", packageName);
+        smallIcon = r.getIdentifier("music_control_icon", "drawable", packageName);
         if(smallIcon == 0) smallIcon = r.getIdentifier("play", "drawable", packageName);
+    }
+    
+    public void setCustomNotificationIcon(String resourceName) {
+        Resources r = context.getResources();
+        String packageName = context.getPackageName();
+
+        customIcon = r.getIdentifier(resourceName, "drawable", packageName);
     }
 
     public void updateActions(long mask, Map<String, Integer> options) {
@@ -77,8 +85,8 @@ public class MusicControlNotification {
         } else { // NotificationClose.NEVER
             builder.setOngoing(true); 
         }
-
-        builder.setSmallIcon(smallIcon);
+        
+        builder.setSmallIcon(customIcon != 0 ? customIcon : smallIcon);
 
         // Open the app when the notification is clicked
         String packageName = context.getPackageName();

android/src/main/java/com/tanguyantoine/react/MusicControlModule.java
@@ -4,6 +4,7 @@ import android.content.ComponentCallbacks2;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -53,7 +54,7 @@ public class MusicControlModule extends ReactContextBaseJavaModule implements Co
     private boolean isPlaying = false;
     private long controls = 0;
     protected int ratingType = RatingCompat.RATING_PERCENTAGE;
-
+    
     public NotificationClose notificationClose = NotificationClose.PAUSED;
 
     public MusicControlModule(ReactApplicationContext context) {
@@ -173,6 +174,7 @@ public class MusicControlModule extends ReactContextBaseJavaModule implements Co
         String date = metadata.hasKey("date") ? metadata.getString("date") : null;
         long duration = metadata.hasKey("duration") ? (long)(metadata.getDouble("duration") * 1000) : 0;
         int notificationColor = metadata.hasKey("color") ? metadata.getInt("color") : NotificationCompat.COLOR_DEFAULT;
+        String notificationIcon = metadata.hasKey("notificationIcon") ? metadata.getString("notificationIcon") : null;
 
         RatingCompat rating;
         if(metadata.hasKey("rating")) {
@@ -204,6 +206,8 @@ public class MusicControlModule extends ReactContextBaseJavaModule implements Co
         nb.setContentText(artist);
         nb.setContentInfo(album);
         nb.setColor(notificationColor);
+        
+        notification.setCustomNotificationIcon(notificationIcon);
 
         if(metadata.hasKey("artwork")) {
             String artwork = null;

commit 761813acf0d7290b8bf7bca8f885115a41736485

Fixed crash introduced by #88

android/src/main/java/com/tanguyantoine/react/MusicControlNotification.java
@@ -40,6 +40,11 @@ public class MusicControlNotification {
     }
     
     public void setCustomNotificationIcon(String resourceName) {
+        if(resourceName == null) {
+            customIcon = 0;
+            return;
+        }
+        
         Resources r = context.getResources();
         String packageName = context.getPackageName();
 