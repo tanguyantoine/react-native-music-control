6b807f8a88c1fbbe94bfedefc2830a44cb5e4dce
Kesha Antonov <innokenty.longway@gmail.com>
2017-11-16 13:37:16 +0300

    refactor to 1 file. DRY

--- index.js
+++ index.js
@@ -4,22 +4,33 @@
  */
 'use strict';
 
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  DeviceEventEmitter,
+  Platform
+} from 'react-native';
 const NativeMusicControl = NativeModules.MusicControlManager;
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
+const IS_ANDROID = Platform.OS === 'android';
 
 /**
  * High-level docs for the MusicControl iOS API can be written here.
  */
 var handlers = {};
 var listenerOfNativeMusicControl = null;
+const RN_MUSIC_CONTROL_EVENT_NAME = 'RNMusicControlEvent';
 
 var formatInfo = function(info){
   // NOTE: CHECK IF WE HAVE AN IOS ASSET FROM REACT STYLE IMAGE REQUIRE
   if(info.artwork) {
     info.artwork = resolveAssetSource(info.artwork) || info.artwork;
   }
-  return info
+  return info;
+}
+
+var handleRNMusicControlEvent = function(event){
+  MusicControl.handleCommand(event.name, event.value);
 }
 
 var MusicControl = {
@@ -31,34 +42,31 @@ var MusicControl = {
   STATE_BUFFERING: NativeMusicControl.STATE_PAUSED,
 
   // Rating is not supported on iOS. This is kept here for compatibility
-  RATING_HEART: 0,
-  RATING_THUMBS_UP_DOWN: 0,
-  RATING_3_STARS: 0,
-  RATING_4_STARS: 0,
-  RATING_5_STARS: 0,
-  RATING_PERCENTAGE: 0,
+  RATING_HEART: IS_ANDROID ? NativeMusicControl.RATING_HEART : 0,
+  RATING_THUMBS_UP_DOWN: IS_ANDROID ? NativeMusicControl.RATING_THUMBS_UP_DOWN : 0,
+  RATING_3_STARS: IS_ANDROID ? NativeMusicControl.RATING_3_STARS : 0,
+  RATING_4_STARS: IS_ANDROID ? NativeMusicControl.RATING_4_STARS : 0,
+  RATING_5_STARS: IS_ANDROID ? NativeMusicControl.RATING_5_STARS : 0,
+  RATING_PERCENTAGE: IS_ANDROID ? NativeMusicControl.RATING_PERCENTAGE : 0,
 
   // NOTE: BACKWARDS COMPATIBILITY. USE "updatePlayback" INSTEAD.
   setPlayback: function (info) {
-    info = formatInfo(info);
-    NativeMusicControl.updatePlayback(info);
+    NativeMusicControl.updatePlayback( formatInfo(info) );
   },
   updatePlayback: function(info) {
-    info = formatInfo(info);
-    NativeMusicControl.updatePlayback(info);
+    NativeMusicControl.updatePlayback( formatInfo(info) );
   },
   enableBackgroundMode: function(enable){
-    NativeMusicControl.enableBackgroundMode(enable)
+    NativeMusicControl.enableBackgroundMode(enable);
   },
   setNowPlaying: function(info){
-    info = formatInfo(info);
-    NativeMusicControl.setNowPlaying(info);
+    NativeMusicControl.setNowPlaying( formatInfo(info) );
   },
   resetNowPlaying: function(){
     NativeMusicControl.resetNowPlaying()
   },
   enableControl: function(controlName, bool, options = {}){
-    NativeMusicControl.enableControl(controlName, bool, options || {})
+    NativeMusicControl.enableControl(controlName, bool, options)
   },
   handleCommand: function(commandName, value){
     if(handlers[commandName]){
@@ -67,12 +75,17 @@ var MusicControl = {
   },
   on: function(actionName, cb){
     if ( listenerOfNativeMusicControl == null ) {
-      listenerOfNativeMusicControl = new NativeEventEmitter(NativeMusicControl).addListener(
-        'RNMusicControlEvent',
-        (event) => {
-          MusicControl.handleCommand(event.name, event.value)
-        }
-      );
+      if ( IS_ANDROID ) {
+        listenerOfNativeMusicControl = DeviceEventEmitter.addListener(
+          RN_MUSIC_CONTROL_EVENT_NAME,
+          handleRNMusicControlEvent
+        );
+      } else {
+        listenerOfNativeMusicControl = new NativeEventEmitter(NativeMusicControl).addListener(
+          RN_MUSIC_CONTROL_EVENT_NAME,
+          handleRNMusicControlEvent
+        );
+      }
     }
     handlers[actionName] = cb
   },
