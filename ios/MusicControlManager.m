6de5d539b3ae440af7e67a5489f752984b3a798b
Kesha Antonov <innokenty.longway@gmail.com>
2018-01-11 11:00:16 +0300

    rm duplicate

--- ios/MusicControlManager.m
+++ ios/MusicControlManager.m
@@ -55,11 +55,6 @@ RCT_EXPORT_MODULE()
     return dispatch_get_main_queue();
 }
 
-+ (BOOL)requiresMainQueueSetup
-{
-    return NO;
-}
-
 RCT_EXPORT_METHOD(updatePlayback:(NSDictionary *) originalDetails)
 {
     MPNowPlayingInfoCenter *center = [MPNowPlayingInfoCenter defaultCenter];
