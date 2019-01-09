cd4b82c270e672ce9f37f82fb80da07c6edd7805
roffelund <gustav.haglund@gmail.com>
2019-01-09 09:24:06 +0100

    stopForeground service onTaskRemoved in MusicControlNotification

--- android/src/main/java/com/tanguyantoine/react/MusicControlNotification.java
+++ android/src/main/java/com/tanguyantoine/react/MusicControlNotification.java
@@ -202,6 +202,9 @@ public class MusicControlNotification {
             if(MusicControlModule.INSTANCE != null) {
                 MusicControlModule.INSTANCE.destroy();
             }
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                stopForeground(true);
+            }
             stopSelf(); // Stop the service as we won't need it anymore
         }
 
