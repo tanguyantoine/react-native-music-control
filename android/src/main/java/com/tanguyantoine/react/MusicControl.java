5ab831ce9d943b06953f2062c26ec0ac633f13c4
Christian Bach <christianbach@users.noreply.github.com>
2019-02-25 12:20:52 +0100

    Revert createJSModules

--- android/src/main/java/com/tanguyantoine/react/MusicControl.java
+++ android/src/main/java/com/tanguyantoine/react/MusicControl.java
@@ -19,10 +19,6 @@ public class MusicControl implements ReactPackage {
         return modules;
     }
 
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext context) {
         return Collections.emptyList();
