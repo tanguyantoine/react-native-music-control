7b01e1772eade280096293335b104da904f81b11
undefined <undefined>
2019-09-11 13:17:16 +0100

    Added JS compiled files

---
+++ lib/types.d.ts
@@ -0,0 +1,19 @@
+export declare enum Command {
+    play = "play",
+    pause = "pause",
+    stop = "stop",
+    nextTrack = "nextTrack",
+    previousTrack = "previousTrack",
+    seekForward = "seekForward",
+    seekBackward = "seekBackward",
+    seek = "seek",
+    volume = "volume",
+    setRating = "setRating",
+    togglePlayPause = "togglePlayPause",
+    enableLanguageOption = "enableLanguageOption",
+    disableLanguageOption = "disableLanguageOption",
+    skipForward = "skipForward",
+    skipBackward = "skipBackward",
+    changePlaybackPosition = "changePlaybackPosition",
+    closeNotification = "closeNotification"
+}
