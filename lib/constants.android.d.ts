7b8592b2aa9426e887f6c8d4da0e5580802454c2
Caue <cauep@Caues-Mac.local>
2020-02-25 16:58:55 -0300

    - replace prepack with prepublish script - add lib dir back to the repo

---
+++ lib/constants.android.d.ts
@@ -0,0 +1,14 @@
+declare const _default: {
+    STATE_PLAYING: any;
+    STATE_PAUSED: any;
+    STATE_ERROR: any;
+    STATE_STOPPED: any;
+    STATE_BUFFERING: any;
+    RATING_HEART: number;
+    RATING_THUMBS_UP_DOWN: number;
+    RATING_3_STARS: number;
+    RATING_4_STARS: number;
+    RATING_5_STARS: number;
+    RATING_PERCENTAGE: number;
+};
+export default _default;
